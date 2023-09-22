6f01f5cdae021d9586bb904ae974d36a9fd51aa8
Dipak <dipakdev094@gmail.com>
2023-09-22 12:27:40 +0530

    API - getting a single blog by blog-id

---
+++ backend/src/middlewares/blog-middlewares.js
@@ -0,0 +1,36 @@
+const { ObjectId } = require("mongodb");
+
+const checkIdMiddleware = (req, res, next) => {
+  const idToCheck = req.params.id;
+
+  if (!idToCheck)
+    return res.status(400).json({ success: false, message: "Id is required" });
+  else if (ObjectId.isValid(idToCheck)) return next();
+  return res.status(400).json({ success: false, message: "Invalid post id" });
+};
+
+const createBlogValidation = (req, res, next) => {
+  const { title, authorName, description } = req.body;
+  if (!title)
+    return res
+      .status(400)
+      .json({ success: false, message: "Title is required" });
+  else if (!authorName)
+    return res
+      .status(400)
+      .json({ success: false, message: "Author name is required" });
+  else if (!description)
+    return res
+      .status(400)
+      .json({ success: false, message: "Description is required" });
+  else if (!req.file)
+    return res
+      .status(400)
+      .json({ success: false, message: "Image file is required" });
+  return next();
+};
+
+module.exports = {
+  checkIdMiddleware,
+  createBlogValidation,
+};
--- backend/src/routers/blog-routers.js
+++ backend/src/routers/blog-routers.js
@@ -1,7 +1,14 @@
 const blogRoute = require("express").Router();
-const { getAllBlogController } = require("../controllers/blog-controllers");
+const {
+  getAllBlogController,
+  getOneBlogController,
+} = require("../controllers/blog-controllers");
+const { checkIdMiddleware } = require("../middlewares/blog-middlewares");
 
 // GET /api/blog - getting all blog
 blogRoute.get("/", getAllBlogController);
+
+// GET /api/blog/:id - getting a specific blog
+blogRoute.get("/:id", checkIdMiddleware, getOneBlogController);
 
 module.exports = blogRoute;
